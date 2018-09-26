7cfa1ece39792c389809940b017bca3e56e5f5c5
patrick_tucker <patrick.tucker@macefusion.com>
2018-09-26 13:27:26 -0400

    Extract/simplify score calcuations.

--- TennisGameRefactoring/src/main/java/TennisGame1.java
+++ TennisGameRefactoring/src/main/java/TennisGame1.java
@@ -20,7 +20,7 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        String score = "";
+        String score;
         int tempScore=0;
         if (m_score1==m_score2)
         {
@@ -28,16 +28,27 @@ public class TennisGame1 implements TennisGame {
         }
         else if (m_score1>=4 || m_score2>=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            score = getOtherScore(m_score1, m_score2);
         }
         else
         {
             score = getPlayerScore(m_score1) + "-" + getPlayerScore(m_score2);
         }
+        return score;
+    }
+
+    private String getOtherScore(int m_score1, int m_score2) {
+        int minusResult = m_score1-m_score2;
+
+        return getOtherScore(minusResult);
+    }
+
+    private String getOtherScore(int minusResult) {
+        String score;
+        if (minusResult==1) score ="Advantage player1";
+        else if (minusResult ==-1) score ="Advantage player2";
+        else if (minusResult>=2) score = "Win for player1";
+        else score ="Win for player2";
         return score;
     }
 
